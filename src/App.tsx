e1c7ef677f0b3056840e3bf76860983c7d12af48
Thiago Albuquerque <thiago.albuquerque@iterative.com.br>
2025-08-27 12:07:53 -0300

    [update] Criado componentes de Header e Home. Tema configurado.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import { useState } from 'react'
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+import { Router } from "./Router";
+import { BrowserRouter } from "react-router-dom";
 
 export function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <h1>Hello, world!</h1>
-  )
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
